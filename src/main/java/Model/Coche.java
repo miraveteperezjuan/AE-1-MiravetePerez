bb9d4cf639559cb02b44fb9308e3581c251b5e89
miravetejuam <juaaanmiravete@gmail.com>
2024-11-19 17:39:25 +0100

    update1

---
+++ src/main/java/Model/Coche.java
@@ -0,0 +1,33 @@
+package Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coche implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private int id;
+    private String matricula;
+    private String marca;
+    private String modelo;
+    private String color;
+
+    @Override
+    public String toString() {
+        return "Coche{" +
+                "id=" + id +
+                ", matricula='" + matricula + '\'' +
+                ", marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
